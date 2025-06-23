b93ef2e29a256ecf6b2b565f1bd5f400ce3c46f6
Gentrit <gentritzheg@gmail.com>
2025-06-23 16:51:48 +0200

    feat(ui): add DropdownMenu component for navigation

---
+++ src/ui/DropdownMenu.tsx
@@ -0,0 +1,47 @@
+import { NavLink } from "react-router";
+
+function DropdownMenu() {
+  return (
+    <li className="group relative">
+      <button className="transition-colors duration-300 hover:text-green-500">
+        Manga
+      </button>
+      <div className="absolute hidden w-60 space-y-2 bg-neutral-800 group-hover:block">
+        <div className="px-3 py-2">
+          <NavLink
+            className="block rounded-md px-1.5 py-0.5 transition-colors duration-300 hover:bg-green-500"
+            to="/top/manga"
+          >
+            Top Manga
+          </NavLink>
+          <NavLink
+            className="block rounded-md px-1.5 py-0.5 transition-colors duration-300 hover:bg-green-500"
+            to="/top/publishing"
+          >
+            Top Publishing
+          </NavLink>
+          <NavLink
+            className="block rounded-md px-1.5 py-0.5 transition-colors duration-300 hover:bg-green-500"
+            to="/top/upcoming"
+          >
+            Top Upcoming
+          </NavLink>
+          <NavLink
+            className="block rounded-md px-1.5 py-0.5 transition-colors duration-300 hover:bg-green-500"
+            to="/top/bypopularity"
+          >
+            Most Popular
+          </NavLink>
+          <NavLink
+            className="block rounded-md px-1.5 py-0.5 transition-colors duration-300 hover:bg-green-500"
+            to="/top/favorite"
+          >
+            Most Favorited
+          </NavLink>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default DropdownMenu;
