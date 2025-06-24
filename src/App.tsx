41730a4827cbe2f2fbc7575b26bf1c4db12c74d2
Gentrit <gentritzheg@gmail.com>
2025-06-24 15:56:38 +0200

    feat(app): add routing and react-query configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import AppLayout from "./ui/AppLayout";
+import Home from "./pages/Home";
+import Top from "./pages/Top";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/top/:type" element={<Top />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
