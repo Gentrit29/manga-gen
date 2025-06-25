4a036504d74d7fbe6e49e7a2852b699b6e58fd0e
Gentrit <gentritzheg@gmail.com>
2025-06-25 16:34:41 +0200

    feat(types): add manga type

---
+++ src/types/manga.ts
@@ -0,0 +1,43 @@
+type Images = {
+  webp: {
+    image_url: string;
+    small_image_url: string;
+    large_image_url: string;
+  };
+};
+
+type Author = {
+  mal_id: number;
+  name: string;
+  url: string;
+};
+
+type Published = {
+  string: string;
+};
+
+export type Genre = {
+  mal_id: number;
+  name: string;
+  url: string;
+};
+
+export type Manga = {
+  mal_id: number;
+  url?: string;
+  images: Images;
+  title: string;
+  title_japanese: string;
+  type: string;
+  score: number;
+  scored_by: number;
+  rank: number;
+  volumes: number;
+  chapters: number;
+  favorites: number;
+  synopsis: string;
+  published: Published;
+  status: string;
+  authors: Author[];
+  genres: Genre[];
+};
