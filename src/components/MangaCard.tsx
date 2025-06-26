5bc188fdc9fa1f6db3be35c6b3abbb3a3ceea558
Gentrit <gentritzheg@gmail.com>
2025-06-26 16:25:52 +0200

    feat(components): add MangaRandom and MangaCard

---
+++ src/components/MangaCard.tsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router";
+
+import type { Manga } from "../types/manga";
+
+type MangaCardProps = {
+  manga: Manga;
+  index: number;
+};
+
+function MangaCard({ manga, index }: MangaCardProps) {
+  return (
+    <Link to={`/detail/${manga.mal_id}`}>
+      <div className="group relative">
+        <img
+          className="h-80 w-60 rounded-lg brightness-90"
+          src={manga.images.webp.image_url}
+        />
+        <div className="absolute right-0 bottom-0 left-0 h-32 rounded-b-lg bg-gradient-to-t from-black/70 to-transparent" />
+        <div className="absolute inset-3 flex flex-col justify-between">
+          <h3 className="w-fit rounded-lg bg-gradient-to-r from-green-500/70 to-emerald-500/70 px-1 py-2 text-lg font-light text-white">
+            #{index + 1}
+          </h3>
+          <h3 className="font-light text-white">{manga.title}</h3>
+        </div>
+        <div className="absolute inset-0 z-10 hidden h-full rounded-lg bg-neutral-900/90 p-2 text-white shadow-xl transition-all duration-300 group-hover:block">
+          <h3 className="line-clamp-2 text-lg font-semibold">{manga.title}</h3>
+          <div className="mt-2 mb-2 flex flex-wrap items-center gap-2 text-sm text-gray-400">
+            <span className="rounded bg-neutral-800 px-2 py-1">
+              {manga.type}
+            </span>
+            {manga.volumes && (
+              <span className="rounded bg-neutral-800 px-2 py-1">
+                {manga.volumes} vol.
+              </span>
+            )}
+            {manga.chapters && (
+              <span className="rounded bg-neutral-800 px-2 py-1">
+                {manga.chapters} ch.
+              </span>
+            )}
+          </div>
+          <p className="mb-3 line-clamp-3 text-sm text-gray-300">
+            {manga.synopsis}
+          </p>
+          <div className="mb-2 text-sm text-gray-400">
+            <p>Aired: {manga.published.string}</p>
+            <p>Status: {manga.status}</p>
+          </div>
+          <div className="flex flex-wrap gap-1 text-xs">
+            {manga.genres.slice(0, 3).map((g) => (
+              <span
+                key={g.mal_id}
+                className="rounded bg-green-600/20 px-2 py-1 text-green-300"
+              >
+                {g.name}
+              </span>
+            ))}
+            {manga.genres.length > 3 && (
+              <span className="rounded bg-green-600/20 px-2 py-1 text-green-300">
+                +{manga.genres.length - 3} more
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default MangaCard;
--- src/components/MangaRandom.tsx
+++ src/components/MangaRandom.tsx
@@ -1,5 +1,27 @@
+import MangaCard from "./MangaCard";
+
+import { useRandomManga } from "../hooks/useRandomManga";
+
+import type { Manga } from "../types/manga";
+
 function MangaRandom() {
-  return <div>MangaRandom</div>;
+  const { isLoading, error, randomManga } = useRandomManga();
+
+  if (isLoading) return null;
+  if (error) return null;
+  return (
+    <section className="mx-20 mt-15 space-y-4">
+      <div className="flex items-center space-x-4">
+        <div className="h-10 w-1.5 rounded-sm bg-green-500"></div>
+        <h2 className="text-2xl font-bold text-white">Random Manga Picks</h2>
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+        {randomManga?.map((manga: Manga, idx: number) => (
+          <MangaCard key={manga.mal_id} manga={manga} index={idx} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default MangaRandom;
