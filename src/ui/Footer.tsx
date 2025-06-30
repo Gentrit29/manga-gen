05e1d213a82bec1e0f835ded22d41aaae1c35889
Gentrit <gentritzheg@gmail.com>
2025-06-30 16:19:49 +0200

    feat(footer): add layout and styles

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,5 +1,13 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="mx-20 mt-15 flex flex-col">
+      <div className="flex items-center justify-center border-t border-white py-5">
+        <span className="text-xl text-white">
+          Developed By <span className="underline decoration-double">GEN</span>
+        </span>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
