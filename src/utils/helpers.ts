756d504757824638a27deb13315cabe8bdd249fd
Gentrit <gentritzheg@gmail.com>
2025-06-30 16:27:24 +0200

    feat(utils): add removeDuplicateGenresTag function (fix api inconsistency)

---
+++ src/utils/helpers.ts
@@ -0,0 +1,10 @@
+import type { Genre } from "../types/manga";
+
+export const removeDuplicateGenresTag = (genres: Genre[]) => {
+  const seen = new Set<string>();
+  return genres.filter((genre) => {
+    if (seen.has(genre.name)) return false;
+    seen.add(genre.name);
+    return true;
+  });
+};
