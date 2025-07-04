0b9ce09565bad5f392442840c4c00517390b621e
Gentrit <gentritzheg@gmail.com>
2025-07-04 13:58:58 +0200

    feat(detail): add MangaCharacters and AboutSection components; update logic

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -3,22 +3,36 @@ import { useMangaFullById } from "../hooks/useMangaFullById";
 import MangaHeader from "../components/MangaHeader";
 import MangaRecommendations from "../components/MangaRecommendations";
 import { useMangaRecommendations } from "../hooks/useMangaRecommendations";
+import { useMangaCharacters } from "../hooks/useMangaCharacters";
+
+import MangaCharacters from "../components/MangaCharacters";
+import AboutSection from "../components/AboutSection";
 
 function Detail() {
   const { id } = useParams();
+
   const { isLoading, error, mangaFull } = useMangaFullById(Number(id));
+
   const {
     isLoading: isRecommendationsLoading,
     error: recommendationsError,
     mangaRecommendations,
   } = useMangaRecommendations(Number(id));
 
-  if (isLoading || isRecommendationsLoading) return null;
-  if (error || recommendationsError) return null;
+  const {
+    isLoading: isCharactersLoading,
+    error: charactersError,
+    mangaCharacters,
+  } = useMangaCharacters(Number(id));
+
+  if (isLoading || isRecommendationsLoading || isCharactersLoading) return null;
+  if (error || recommendationsError || charactersError) return null;
 
   return (
     <>
       <MangaHeader params={mangaFull} />
+      <MangaCharacters params={mangaCharacters} />
+      <AboutSection />
       <MangaRecommendations params={mangaRecommendations} />
     </>
   );
