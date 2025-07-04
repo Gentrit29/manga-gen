34041495b25e393473542f6d14042fb993e708b5
Gentrit <gentritzheg@gmail.com>
2025-07-04 14:51:56 +0200

    feat(top): temporary layout and logic

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -6,32 +6,19 @@ import type { Manga } from "../types/manga";
 import Pagination from "../ui/Pagination";
 import { useEffect, useState } from "react";
 
-const NAV_LABELS: Record<string, { label: string; desc: string }> = {
-  manga: {
-    label: "Top Manga",
-    desc: "The highest-ranked manga of all time",
-  },
-  publishing: {
-    label: "Top Publishing",
-    desc: "Currently ongoing manga series",
-  },
-  upcoming: {
-    label: "Top Upcoming",
-    desc: "Most anticipated manga releases coming soon",
-  },
-  bypopularity: {
-    label: "Most Popular",
-    desc: "Manga that have captivated readers over time",
-  },
-  favorite: {
-    label: "Most Favorited",
-    desc: "Fan-favorite manga picks of all time",
-  },
-};
+const tabs = [
+  { label: "Manga", value: "manga" },
+  { label: "Manhwa", value: "manhwa" },
+  { label: "Manhua", value: "manhua" },
+  { label: "Novel", value: "novel" },
+  { label: "Light Novel", value: "lightnovel" },
+];
 
+//Reminder to clean up this component (prob make small and reusable components)
 function Top() {
   const { category } = useParams();
   const [nextPage, setNextPage] = useState(1);
+  const [selectedTab, setSelectedTab] = useState("manga");
 
   useEffect(() => {
     setNextPage(1);
@@ -40,10 +27,34 @@ function Top() {
   const { isLoading, error, topManga } = useTopManga(
     category || "manga",
     nextPage,
+    selectedTab,
   );
 
   if (isLoading) return null;
   if (error) return null;
+
+  const NAV_LABELS: Record<string, { label: string; desc: string }> = {
+    manga: {
+      label: `Top ${selectedTab.charAt(0).toUpperCase() + selectedTab.slice(1).toLowerCase()}`,
+      desc: `The highest-ranked ${selectedTab} of all time`,
+    },
+    publishing: {
+      label: "Top Publishing",
+      desc: "Currently ongoing manga series",
+    },
+    upcoming: {
+      label: "Top Upcoming",
+      desc: "Most anticipated manga releases coming soon",
+    },
+    bypopularity: {
+      label: "Most Popular",
+      desc: "Manga that have captivated readers over time",
+    },
+    favorite: {
+      label: "Most Favorited",
+      desc: "Fan-favorite manga picks of all time",
+    },
+  };
 
   const TOP_LABEL =
     category && NAV_LABELS[category]
@@ -63,6 +74,21 @@ function Top() {
         }
       />
       <div className="space-y-6">
+        <div className="mt-4 flex space-x-6 border-b-1 border-gray-200 text-xl text-white">
+          {tabs.map((tab) => (
+            <button
+              className={`pb-2 transition-all ${
+                selectedTab === tab.value
+                  ? "border-b-1 border-gray-200 text-white"
+                  : "hover: border-gray-200 text-gray-400 hover:border-b-1 hover:text-white"
+              }`}
+              key={tab.value}
+              onClick={() => setSelectedTab(tab.value)}
+            >
+              {tab.label}
+            </button>
+          ))}
+        </div>
         <div className="grid grid-cols-5 gap-4">
           {topManga?.data.map((manga: Manga, idx: number) => (
             <MangaCard key={manga.mal_id} manga={manga} index={idx} />
