b321ed90f3a7e8f7d0a3c1d2388a2f9153e83d61
Gentrit <gentritzheg@gmail.com>
2025-07-07 13:49:58 +0200

    feat(genre): update component to get name from URL and display it

--- src/pages/Genre.tsx
+++ src/pages/Genre.tsx
@@ -1,22 +1,31 @@
-import { useParams } from "react-router";
+import { useLocation, useParams } from "react-router";
 import { useMangaSearchByGenre } from "../hooks/useMangaSearchByGenre";
 import MangaCard from "../components/MangaCard";
 import type { Manga } from "../types/manga";
 
 import { useState } from "react";
 import Pagination from "../ui/Pagination";
+import { formatGenreNameForDisplay } from "../utils/helpers";
 
 function Genre() {
   const { id } = useParams();
+  const location = useLocation();
+
+  const genreName = new URLSearchParams(location.search).get("name");
+
+  const formattedName = genreName ? formatGenreNameForDisplay(genreName) : "";
 
   const [nextPage, setNextPage] = useState(1);
 
   const { mangaList } = useMangaSearchByGenre(Number(id), nextPage);
+
   return (
     <div className="mx-20 my-15 space-y-5">
       <div className="flex items-center space-x-4">
         <div className="h-10 w-1.5 rounded-sm bg-green-500"></div>
-        <h2 className="text-2xl font-bold text-white">Result for test</h2>
+        <h2 className="text-2xl font-bold text-white">
+          Result for {formattedName}
+        </h2>
       </div>
       {mangaList?.data && mangaList.data.length > 0 ? (
         <div className="grid grid-cols-5 gap-4">
