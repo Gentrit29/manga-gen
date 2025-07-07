f8fdc624b25d84e1d0f16f373272b2430ad8814a
Gentrit <gentritzheg@gmail.com>
2025-07-07 15:13:31 +0200

    fix(mangagrid): correct manga prop type and fix manga mapping

--- src/ui/MangaGrid.tsx
+++ src/ui/MangaGrid.tsx
@@ -1,14 +1,14 @@
 import MangaCard from "../components/MangaCard";
-import type { Manga, MangaData } from "../types/manga";
+import type { MangaData } from "../types/manga";
 
 type MangaGridProps = {
-  manga: Manga;
+  manga: MangaData[];
 };
 
 function MangaGrid({ manga }: MangaGridProps) {
   return (
     <div className="grid grid-cols-5 gap-4">
-      {manga?.data.map((manga: MangaData, idx: number) => (
+      {manga?.map((manga: MangaData, idx: number) => (
         <MangaCard key={manga.mal_id} manga={manga} index={idx} />
       ))}
     </div>
