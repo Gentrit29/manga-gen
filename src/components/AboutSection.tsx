c0856aa808fdb871379d46a054552637cb934278
Gentrit <gentritzheg@gmail.com>
2025-07-08 16:03:45 +0200

    style(aboutsection): improve responsive design for component

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,15 +3,15 @@ import { IoLogoGithub } from "react-icons/io5";
 function AboutSection() {
   return (
     <section
-      className="relative mx-20 mt-15 bg-cover bg-center bg-no-repeat py-20 text-white brightness-110"
+      className="relative mx-5 mt-15 bg-cover bg-center bg-no-repeat py-20 text-white brightness-110 lg:mx-20 xl:mx-50"
       style={{
         backgroundImage: 'url("/about-section/aboutsectionbanner.jpg")',
       }}
     >
       <div className="absolute inset-0 z-0 bg-gradient-to-r from-neutral-900 via-transparent to-neutral-900" />
       <div className="relative z-10 mx-auto max-w-3xl text-center">
-        <h2 className="mb-10 text-3xl font-bold">Manga GEN</h2>
-        <p className="text-lg">
+        <h2 className="mb-10 text-2xl font-bold md:text-3xl">Manga GEN</h2>
+        <p className="px-5 text-sm md:text-lg">
           Discover everything about manga and more — search, explore top
           recommendations, check out community favorites from MyAnimeList, and
           much more — all in one place. This website is powered by the{" "}
