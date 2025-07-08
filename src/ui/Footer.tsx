3dcade3d24ac65dff65935638e8f50bb66c4b1a9
Gentrit <gentritzheg@gmail.com>
2025-07-08 16:06:21 +0200

    style(footer): improve responsive design for component

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <footer className="mx-20 mt-15 flex flex-col">
+    <footer className="mx-5 mt-15 flex flex-col lg:mx-20 xl:mx-50">
       <div className="flex items-center justify-center border-t border-white py-5">
-        <span className="text-xl text-white">
+        <span className="text-base text-white md:text-xl">
           Developed By <span className="underline decoration-double">GEN</span>
         </span>
       </div>
