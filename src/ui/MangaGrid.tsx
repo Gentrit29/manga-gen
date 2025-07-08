e5d3719ade5ef928d73092fbeb7b1aa4650df62a
Gentrit <gentritzheg@gmail.com>
2025-07-08 16:04:36 +0200

    style(mangagrid): improve responsive design for component

--- src/ui/MangaGrid.tsx
+++ src/ui/MangaGrid.tsx
@@ -7,7 +7,7 @@ type MangaGridProps = {
 
 function MangaGrid({ manga }: MangaGridProps) {
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       {manga?.map((manga: MangaData, idx: number) => (
         <MangaCard key={manga.mal_id} manga={manga} index={idx} />
       ))}
