d99c5c605146b2b3af28e1a485968abceb96d6e3
Gentrit <gentritzheg@gmail.com>
2025-07-10 17:04:11 +0200

    feat(randommanga): add refetch logic and isFetching loading state to RandomManga component

--- src/pages/Home/MangaRandom.tsx
+++ src/pages/Home/MangaRandom.tsx
@@ -1,6 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Autoplay, Navigation } from "swiper/modules";
 
+import { IoReload } from "react-icons/io5";
+
 import MangaCard from "../../components/MangaCard";
 
 import MangaGrid from "../../ui/MangaGrid";
@@ -11,18 +13,29 @@ import { useRandomManga } from "../../hooks/useRandomManga";
 import type { MangaData } from "../../types/manga";
 
 function MangaRandom() {
-  const { isLoading, randomManga } = useRandomManga();
+  const { isLoading, isFetching, refetch, randomManga } = useRandomManga();
 
   // if (error) return null;
 
   return (
     <section className="mx-5 mt-15 space-y-4 lg:mx-20 2xl:mx-40">
-      <div className="flex items-center space-x-2 md:space-x-4">
-        <div className="h-10 w-1.5 rounded-sm bg-green-500"></div>
-        <h2 className="text-2xl font-bold text-white">Random Manga Picks</h2>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <div className="h-10 w-1.5 rounded-sm bg-green-500" />
+          <h2 className="text-2xl font-bold text-white">Random Manga Picks</h2>
+        </div>
+        <button
+          disabled={isFetching}
+          className="cursor-pointer"
+          onClick={() => refetch()}
+        >
+          <IoReload
+            className={`h-6 w-6 text-white transition-colors duration-300 hover:text-green-500 ${isFetching ? "animate-spin" : ""}`}
+          />
+        </button>
       </div>
       <div className="hidden xl:block">
-        {isLoading ? (
+        {isLoading || isFetching ? (
           <SkeletonGrid elements={5} />
         ) : (
           randomManga &&
