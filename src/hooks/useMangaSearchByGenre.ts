56b30d5959c9f62ae77856c2787162549f6384c1
Gentrit <gentritzheg@gmail.com>
2025-07-10 16:42:29 +0200

    feat(hooks): return loading and error states from useMangaSearchByGenre

--- src/hooks/useMangaSearchByGenre.ts
+++ src/hooks/useMangaSearchByGenre.ts
@@ -2,10 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getMangaSearchByGenre } from "../api/mangaApi";
 
 export function useMangaSearchByGenre(genres: number, page = 1) {
-  const { data: mangaList } = useQuery({
+  const {
+    isLoading,
+    error,
+    data: mangaList,
+  } = useQuery({
     queryKey: ["mangaByGenre", genres, page],
     queryFn: () => getMangaSearchByGenre(genres, page),
   });
 
-  return { mangaList };
+  return { isLoading, error, mangaList };
 }
