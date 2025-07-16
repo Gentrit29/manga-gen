affdb2ee2fd3bf333b5a789eaa4e6ae32d3f1de0
Gentrit <gentritzheg@gmail.com>
2025-07-16 14:57:22 +0200

    feat(api): update API logic and add error handling for data fetching

---
+++ src/api/handleError.ts
@@ -0,0 +1,18 @@
+export async function handleError(url: string, errorMessage: string) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(errorMessage || "An error has occurred");
+    }
+
+    const json = await res.json();
+    if (!json?.data || json.data.length === 0) {
+      throw new Error(errorMessage || "No data found");
+    }
+
+    return json;
+  } catch (err) {
+    console.error(err);
+    throw new Error(errorMessage || "An error has occurred");
+  }
+}
--- src/api/mangaApi.ts
+++ src/api/mangaApi.ts
@@ -1,173 +1,101 @@
+import type { Route } from "../types/manga";
+import { routeMap } from "../utils/routing";
+import { handleError } from "./handleError";
+
 const JIKAN_API_URL = import.meta.env.VITE_JIKAN_API_BASE_URL;
 
 export const getTop5Manga = async () => {
-  const res = await fetch(`${JIKAN_API_URL}/top/manga?limit=5&sfw=true`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch top manga");
-  }
+  const url = `${JIKAN_API_URL}/top/manga?limit=5&sfw=true`;
 
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no manga found ");
-  }
+  const json = await handleError(url, `Failed to fetch top manga: ${url}`);
 
   return json.data;
 };
 
 export const getRandomMangas = async () => {
   const randomPage = Math.floor(Math.random() * 100) + 1;
-  const res = await fetch(
-    `${JIKAN_API_URL}/manga?sfw=true&genres_exclude=49,65&min_score=7&limit=5&page=${randomPage}`,
-  );
 
-  if (!res.ok) {
-    throw new Error("Failed fetching random mangas");
-  }
+  const url = `${JIKAN_API_URL}/manga?sfw=true&genres_exclude=49,65&min_score=7&limit=5&page=${randomPage}`;
 
-  const json = await res.json();
+  const json = await handleError(url, `Failed fetching random mangas: ${url}`);
 
   return json.data;
 };
 
 export const getTopManga = async (
-  category: string,
+  category: Route,
   page: number = 1,
   type: string = "",
 ) => {
-  const map = [
-    {
-      route: "manga",
-      params: `&order_by=score&type=${type}&sort=desc`,
-    },
-    {
-      route: "publishing",
-      params: `&type=${type}&status=publishing&sort=desc`,
-    },
-    {
-      route: "upcoming",
-      params: `&type=${type}&status=upcoming&sort=asc`,
-    },
-    {
-      route: "bypopularity",
-      params: `&order_by=popularity&type=${type}&sort=desc`,
-    },
-    { route: "favorite", params: `&order_by=favorites&type=${type}&sort=desc` },
-  ];
-
-  const routeConfig = map.find((r) => r.route === category);
-
-  if (!routeConfig) throw new Error("Route not found");
+  const routeConfig = routeMap[category];
 
   const baseURL = `${JIKAN_API_URL}/manga?sfw=true&genres_exclude=49,65&page=${page}`;
 
-  const url = `${baseURL}${routeConfig.params}`;
+  const url = `${baseURL}${routeConfig.params(type)}`;
 
-  const res = await fetch(url);
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch top manga");
-  }
-
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no manga found ");
-  }
+  const json = await handleError(url, `Failed to fetch top manga: ${url}`);
 
   return { data: json.data, pagination: json.pagination };
 };
 
 export const getMangaGenres = async () => {
-  const res = await fetch(`${JIKAN_API_URL}/genres/manga?filter=genres`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch manga genres");
-  }
+  const url = `${JIKAN_API_URL}/genres/manga?filter=genres`;
 
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no genres found ");
-  }
+  const json = await handleError(url, `Failed to fetch manga genres: ${url}`);
 
   return json.data;
 };
 
 export const getAllMangaGenres = async () => {
-  const res = await fetch(`${JIKAN_API_URL}/genres/manga`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch manga genres");
-  }
+  const url = `${JIKAN_API_URL}/genres/manga`;
 
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no genres found ");
-  }
+  const json = await handleError(url, `Failed to fetch manga genres: ${url}`);
 
   return json.data;
 };
 
 export const getMangaSearch = async (query: string, page: number = 1) => {
-  const res = await fetch(
-    `${JIKAN_API_URL}/manga?q=${query}&sfw&genres_exclude=49,65&page=${page}`,
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch manga by query");
-  }
+  const url = `${JIKAN_API_URL}/manga?q=${query}&sfw&genres_exclude=49,65&page=${page}`;
 
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no manga found ");
-  }
+  const json = await handleError(url, `Failed to fetch manga by query: ${url}`);
 
   return { data: json.data, pagination: json.pagination };
 };
 
 export const getMangaFullById = async (id: number) => {
-  const res = await fetch(`${JIKAN_API_URL}/manga/${id}/full`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch manga by id");
-  }
+  const url = `${JIKAN_API_URL}/manga/${id}/full`;
 
-  const json = await res.json();
-  if (!json?.data) {
-    throw new Error(`Failed to fetch data | manga id: ${id}`);
-  }
+  const json = await handleError(url, `Failed to fetch manga by id: ${url}`);
 
   return json.data;
 };
 
 export const getMangaRecommendations = async (id: number) => {
-  const res = await fetch(`${JIKAN_API_URL}/manga/${id}/recommendations`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch recommendations by id");
-  }
+  const url = `${JIKAN_API_URL}/manga/${id}/recommendations`;
 
-  const json = await res.json();
+  const json = await handleError(
+    url,
+    `Failed to fetch recommendations by id: ${url}`,
+  );
 
   return json?.data ?? [];
 };
 
 export const getMangaCharacters = async (id: number) => {
-  const res = await fetch(`${JIKAN_API_URL}/manga/${id}/characters`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch characters by id");
-  }
+  const url = `${JIKAN_API_URL}/manga/${id}/characters`;
 
-  const json = await res.json();
+  const json = await handleError(
+    url,
+    `Failed to fetch characters by id: ${url}`,
+  );
 
   return json?.data ?? [];
 };
 
 export const getMangaSearchByGenre = async (genres: number, page = 1) => {
-  const res = await fetch(
-    `${JIKAN_API_URL}/manga?genres=${genres}&sfw&page=${page}`,
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch manga by genre");
-  }
+  const url = `${JIKAN_API_URL}/manga?genres=${genres}&sfw&page=${page}`;
 
-  const json = await res.json();
-  if (!json?.data || json.data.length === 0) {
-    throw new Error("Failed to fetch data | no manga found ");
-  }
+  const json = await handleError(url, `Failed to fetch manga by genre: ${url}`);
 
   return { data: json.data, pagination: json.pagination };
 };
