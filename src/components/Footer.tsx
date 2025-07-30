99ca536fd138e6fe2444e574c2917e453c88e50b
Gentrit <gentritzheg@gmail.com>
2025-07-30 15:09:57 +0200

    feat(footer): update text and style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 function Footer() {
   return (
     <footer className="layout flex flex-col">
-      <div className="flex items-center justify-center border-t border-white py-5">
-        <span className="text-base text-white md:text-xl">
-          Developed By <span className="underline decoration-double">GEN</span>
+      <div className="flex justify-center border-t border-white py-5 text-white">
+        <span>
+          Manga GEN - Powered by{" "}
+          <span className="text-green-500">Jikan API</span>
         </span>
       </div>
     </footer>
