30a2046e091ede3415ce31a977f840050f44530f
Gentrit <gentritzheg@gmail.com>
2025-07-30 14:14:40 +0200

    feat(lib): add static MAIN_GENRE list

---
+++ src/lib/genres.ts
@@ -0,0 +1,92 @@
+export const MAIN_GENRE = [
+  {
+    mal_id: 1,
+    name: "Action",
+    url: "https://myanimelist.net/manga/genre/1/Action",
+  },
+  {
+    mal_id: 2,
+    name: "Adventure",
+    url: "https://myanimelist.net/manga/genre/2/Adventure",
+  },
+  {
+    mal_id: 5,
+    name: "Avant Garde",
+    url: "https://myanimelist.net/manga/genre/5/Avant_Garde",
+  },
+  {
+    mal_id: 46,
+    name: "Award Winning",
+    url: "https://myanimelist.net/manga/genre/46/Award_Winning",
+  },
+  {
+    mal_id: 28,
+    name: "Boys Love",
+    url: "https://myanimelist.net/manga/genre/28/Boys_Love",
+  },
+  {
+    mal_id: 4,
+    name: "Comedy",
+    url: "https://myanimelist.net/manga/genre/4/Comedy",
+  },
+  {
+    mal_id: 8,
+    name: "Drama",
+    url: "https://myanimelist.net/manga/genre/8/Drama",
+  },
+  {
+    mal_id: 10,
+    name: "Fantasy",
+    url: "https://myanimelist.net/manga/genre/10/Fantasy",
+  },
+  {
+    mal_id: 26,
+    name: "Girls Love",
+    url: "https://myanimelist.net/manga/genre/26/Girls_Love",
+  },
+  {
+    mal_id: 47,
+    name: "Gourmet",
+    url: "https://myanimelist.net/manga/genre/47/Gourmet",
+  },
+  {
+    mal_id: 14,
+    name: "Horror",
+    url: "https://myanimelist.net/manga/genre/14/Horror",
+  },
+  {
+    mal_id: 7,
+    name: "Mystery",
+    url: "https://myanimelist.net/manga/genre/7/Mystery",
+  },
+  {
+    mal_id: 22,
+    name: "Romance",
+    url: "https://myanimelist.net/manga/genre/22/Romance",
+  },
+  {
+    mal_id: 24,
+    name: "Sci-Fi",
+    url: "https://myanimelist.net/manga/genre/24/Sci-Fi",
+  },
+  {
+    mal_id: 36,
+    name: "Slice of Life",
+    url: "https://myanimelist.net/manga/genre/36/Slice_of_Life",
+  },
+  {
+    mal_id: 30,
+    name: "Sports",
+    url: "https://myanimelist.net/manga/genre/30/Sports",
+  },
+  {
+    mal_id: 37,
+    name: "Supernatural",
+    url: "https://myanimelist.net/manga/genre/37/Supernatural",
+  },
+  {
+    mal_id: 45,
+    name: "Suspense",
+    url: "https://myanimelist.net/manga/genre/45/Suspense",
+  },
+];
