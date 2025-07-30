0cdcd4e9e189090d364879f6ed997bc36879db7a
Gentrit <gentritzheg@gmail.com>
2025-07-30 15:34:06 +0200

    fix(mangagrid): update logic on animation to prevent rendering problems on wide screens

--- src/ui/MangaGrid.tsx
+++ src/ui/MangaGrid.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "react-responsive";
 import MangaCard from "../components/MangaCard";
 import { useGridAnimation } from "../hooks/useGridAnimation";
 import type { MangaData } from "../types/manga";
@@ -10,20 +11,25 @@ type MangaGridProps = {
 
 function MangaGrid({ manga }: MangaGridProps) {
   const animation = useGridAnimation();
+  const isWideScreen = useMediaQuery({ minWidth: 1440 });
+  const columns = 5;
   return (
     <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 xl:grid-cols-5">
-      {manga?.map((manga: MangaData, idx: number) => (
-        <motion.div
-          key={manga.mal_id}
-          custom={idx}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0 }}
-          variants={animation}
-        >
-          <MangaCard manga={manga} index={idx} />
-        </motion.div>
-      ))}
+      {manga?.map((manga: MangaData, idx: number) => {
+        const isFirstRow = idx < columns;
+        return (
+          <motion.div
+            key={manga.mal_id}
+            custom={idx}
+            initial={isWideScreen && isFirstRow ? "visible" : "hidden"}
+            whileInView={isWideScreen && isFirstRow ? undefined : "visible"}
+            viewport={{ once: true, amount: 0 }}
+            variants={animation}
+          >
+            <MangaCard manga={manga} index={idx} />
+          </motion.div>
+        );
+      })}
     </div>
   );
 }
