d6b9ba022a0858a01926000cc4d4591ca127ef81
Gentrit <gentritzheg@gmail.com>
2025-08-01 18:02:55 +0200

    refactor(mangaheader): update title rendering logic and responsive font sizes

--- src/pages/Detail/MangaHeader.tsx
+++ src/pages/Detail/MangaHeader.tsx
@@ -25,14 +25,9 @@ function MangaHeader({ params, isLoading }: MangaHeaderProps) {
           className="h-70 w-60 rounded-lg md:h-80"
         />
         <div className="mt-4 flex flex-col space-y-2 xl:mt-0">
-          <h1 className="text-4xl font-bold text-white">
-            {params.title_english}
+          <h1 className="text-2xl font-bold text-white md:text-3xl xl:text-4xl">
+            {params.title_english || params.title}
           </h1>
-          {params.title !== params.title_english && (
-            <h2 className="text-lg font-light text-white md:text-xl">
-              ({params.title})
-            </h2>
-          )}
           <div className="flex flex-wrap items-center gap-2 text-base font-light text-white">
             <span className="rounded bg-linear-to-r from-green-500 to-emerald-500 px-2 py-1">
               {params.type}
